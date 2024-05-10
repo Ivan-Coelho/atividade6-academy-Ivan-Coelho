1668726e2d895f27fe837aa22e36fd6f30a0779f
Ivan-Coelho <ivancoelhobap@yahoo.com.br>
2024-05-10 11:24:33 -0300

    melhorias

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
+  projectId: 'qs33z6',
   e2e: {
     specPattern: 'cypress/e2e/**/*.feature',
     baseUrl:'https://rarocrud-frontend-88984f6e4454.herokuapp.com',
